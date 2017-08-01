9598b5c0a28bdd67059515d980a685a54e5c96c6
Zarkwelder <osborne.andrewj@gmail.com>
2017-08-01 11:15:24 -0700

    Misc minor changes

--- app/src/main/java/com/example/zark/baking/MainActivity.java
+++ app/src/main/java/com/example/zark/baking/MainActivity.java
@@ -4,6 +4,7 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
